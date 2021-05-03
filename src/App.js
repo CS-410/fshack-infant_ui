476627995f832a2fcefdf5d0c05c91398b5c3e81
EncryptedCurse <EncryptedCurse@users.noreply.github.com>
2021-05-03 15:13:33 -0400

    2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
-import logo from "./logo.svg";
+import logo from "./chris-logo2.png";
 import React, { useState } from "react";
+import BgImage from "./bg.png";
+import BrainImage from "./brain.svg";
 
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -10,7 +12,6 @@ import Modal from "react-bootstrap/Modal";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
 import Alert from "react-bootstrap/Alert";
 
 function MyVerticallyCenteredModal(props) {
@@ -33,7 +34,7 @@ function MyVerticallyCenteredModal(props) {
               Username
             </Form.Label>
             <Col sm={9}>
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="text" placeholder="chris" />
             </Col>
           </Form.Group>
           <br />
@@ -42,14 +43,18 @@ function MyVerticallyCenteredModal(props) {
               Password
             </Form.Label>
             <Col sm={9}>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="chris1234" />
             </Col>
           </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-        <Button onClick={props.onHide}>Continue</Button>
+        <Button variant="outline-danger" onClick={props.onHide}>
+          Close
+        </Button>
+        <Button variant="success" onClick={props.onHide}>
+          Continue
+        </Button>
       </Modal.Footer>
     </Modal>
   );
@@ -60,28 +65,26 @@ function App() {
 
   return (
     <>
-      <Navbar bg="light" expand="lg" className="py-4">
+      <Navbar bg="light" expand="lg" className="py-3">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand>
             <img
               alt=""
               src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
+              style={{ filter: "brightness(1) invert(1)" }}
+              height="50"
+              className="d-inline-block"
             />{" "}
             InfantFS UI
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="navbar-nav me-auto mb-2 mb-md-0">
-              <Nav.Link className="mx-5" href="#home">
-                Home
-              </Nav.Link>
-              <Nav.Link href="#link">Results</Nav.Link>
+            <Nav className="navbar-nav me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#results">Results</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="#link" onClick={() => setModalShow(true)}>
+              <Nav.Link href="#login" onClick={() => setModalShow(true)}>
                 Login
               </Nav.Link>
             </Nav>
@@ -94,9 +97,12 @@ function App() {
       />
       <br />
       <br />
-      <Container className="align-self-center">
+      <Container
+        className="my-auto d-flex align-middle align-content-middle justify-content-center align-content-center"
+        style={{ display: "flex" }}
+      >
         <Row>
-          <Col xs={6}>
+          <Col xs={7}>
             <h2>Background</h2>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -117,17 +123,33 @@ function App() {
             culpa qui officia deserunt mollit anim id est laborum.
           </Col>
           <Col>
-            <br />
-            <Container style={{ display: "flex" }}>
-              <Alert variant="primary">This is a alert—check it out!</Alert>
-              <Button
-                variant="primary"
-                size="lg"
-                className="m-auto align-content-center"
-                block
-              >
-                Upload a .nii or .dcm dataset
-              </Button>
+            <Container>
+              <Alert variant="primary" className="text-center">
+                <br />
+                <h5>
+                  <b>Current file:</b> None
+                </h5>
+                <br />
+                <img
+                  style={{ filter: "brightness(0) invert(1)" }}
+                  src={BrainImage}
+                  width="50%"
+                />
+                <br />
+                <br />
+                <br />
+                <Button variant="primary" size="lg" block>
+                  Upload a <b>.NII</b> or <b>.DCM</b> dataset
+                </Button>
+                <br />
+                <br />
+                <hr />
+                <div className="d-flex justify-content-center">
+                  <Button variant="success" style={{ width: "100%" }} block>
+                    Continue
+                  </Button>
+                </div>
+              </Alert>
             </Container>
           </Col>
         </Row>
@@ -140,16 +162,29 @@ function App() {
               className="justify-content-end"
             >
               <Nav>
-                <Nav.Link href="#home">GitHub</Nav.Link>
-                <Nav.Link href="#link">ChRIS Project</Nav.Link>
+                <Nav.Link href="https://github.com/CS-410/pl-fshack-infant/">
+                  GitHub
+                </Nav.Link>
+                <Nav.Link href="https://chrisproject.org/">
+                  ChRIS Project
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        <br />
-        footer background image here
-        <br />
-        <br />
+        <div
+          style={{
+            background: `url(${BgImage}) no-repeat`,
+            backgroundSize: "cover",
+          }}
+        >
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
       </div>
     </>
   );
