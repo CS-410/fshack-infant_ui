be65241d18c00cad9e3ff5722e93c5ba9eff5aff
Joyce Quach <jtquach1@gmail.com>
2021-05-05 03:03:23 -0400

    Migrated ChrisClient to TypeScript

--- src/ChrisClient.ts
+++ src/ChrisClient.ts
@@ -1,12 +1,15 @@
 import Client from "@fnndsc/chrisapi";
 
 class ChrisClient {
-	getInstance() {
+	private client: Client;
+	private unauthorizedToken: boolean;
+
+	getInstance(): Client {
 		if (!this.client || this.unauthorizedToken) {
 			const authToken = window.sessionStorage.getItem("authToken") || "";
 			this.unauthorizedToken = authToken === "" ? true : false;
 			this.client = new Client(process.env.REACT_APP_API_URL, {
-				authToken,
+				token: authToken,
 			});
 		}
 		return this.client;
