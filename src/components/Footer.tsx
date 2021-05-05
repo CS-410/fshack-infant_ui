2dcef73a6f3890909e3df4bb112915ff71505045
EncryptedCurse <EncryptedCurse@users.noreply.github.com>
2021-05-04 23:57:49 -0400

    update footer image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
-import footerImage from '../assets/footerImage.png';
+import footerImage from '../assets/nodes.svg';
 
 function Footer(): JSX.Element {
 	return (
