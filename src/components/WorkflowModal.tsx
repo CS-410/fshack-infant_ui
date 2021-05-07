0008817785b33468daae76f2cf811c74a8399e92
EncryptedCurse <EncryptedCurse@users.noreply.github.com>
2021-05-07 13:19:21 -0400

    add (untested) pl-med2img child node to workflow

--- src/components/WorkflowModal.tsx
+++ src/components/WorkflowModal.tsx
@@ -21,11 +21,20 @@ interface IFSHackData extends IPluginCreateData {
 	args: string;
 }
 
+interface IMedImgData extends IPluginCreateData {
+	inputFile: string;
+	outputFileStem: string;
+	sliceToConvert: number;
+}
+
 function WorkflowModal(props: ModalProps): JSX.Element {
 	const [state, setState] = useSharedState();
 
 	async function runWorkflow(): Promise<void> {
 		const client = await ClientSingleton.getInstance();
+		const uploadedFileName = state.uploadedFile.data.fname.split("/").pop();
+
+		// parent node: pl-dircopy
 		const dircopyLookup = await client.getPlugins({ name: "pl-dircopy" });
 		const dircopyPlugin = await dircopyLookup.getItems()[0];
 		const dircopyArguments: IDirCreateData = {
@@ -37,6 +46,22 @@ function WorkflowModal(props: ModalProps): JSX.Element {
 			dircopyArguments
 		);
 
+		// child node: pl-med2img
+		const med2imgLookup = await client.getPlugins({ name: "pl-med2img" });
+		const med2imgPlugin = await med2imgLookup.getItems()[0];
+		const med2imgArguments: IMedImgData = {
+			title: "Input visual",
+			previous_id: dircopyInstance.data.id,
+			inputFile: uploadedFileName,
+			outputFileStem: `${uploadedFileName}.png`,
+			sliceToConvert: 0,
+		};
+		const med2imgInstance = await client.createPluginInstance(
+			med2imgPlugin.data.id,
+			med2imgArguments
+		);
+
+		// child node: pl-fshack-infant
 		const infantfsLookup = await client.getPlugins({
 			name: "pl-fshack-infant",
 		});
@@ -44,7 +69,7 @@ function WorkflowModal(props: ModalProps): JSX.Element {
 		const infantfsArguments: IFSHackData = {
 			previous_id: dircopyInstance.data.id,
 			title: "InfantFS",
-			inputFile: state.uploadedFile.data.fname.split('/').pop(),
+			inputFile: uploadedFileName,
 			outputFile: "test",
 			exec: "recon-all",
 			args: "'{ -all }'",
