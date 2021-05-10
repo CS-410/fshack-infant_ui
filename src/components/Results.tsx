720e0a98925b18283fe7c31e3f148d6ee47b06bf
Wilson Wong <wilson.wong002@umb.edu>
2021-05-09 22:44:37 -0400

    Add sticky footer css

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,10 +1,32 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Table } from "react-bootstrap";
 
 function Results(): JSX.Element {
 	return (
 		<Container className="py-3">
 			<Row>
 				<h1>Results page</h1>
+			</Row>
+			<Row>
+				<Table hover>
+					<thead>
+						<tr>
+							<th>Study</th>
+							<th>Study Date</th>
+							<th>Patient MRN</th>
+							<th>Patient DOB</th>
+							<th>Analysis Created</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Data</td>
+							<td>Data</td>
+							<td>Data</td>
+							<td>Data</td>
+							<td>Data</td>
+						</tr>
+					</tbody>
+				</Table>
 			</Row>
 		</Container>
 	);
