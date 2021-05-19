420eb979858f7109cce84cccb2e6bdacc40414b6
Richard Pan <rpan411@gmail.com>
2021-05-19 17:14:42 -0400

    added correct descriptions to about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,29 +7,45 @@ function About(): JSX.Element {
 			<Row>
 				<Col md={7}>
 					<h1>About Infant FS Hack</h1>
-					<h2>Backgound</h2>
+					<h2>Background</h2>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-						sed do eiusmod tempor incididunt ut labore et dolore
-						magna aliqua. Ut enim ad minim veniam, quis nostrud
-						exercitation ullamco laboris nisi ut aliquip ex ea
-						commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu
-						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-						non proident, sunt in culpa qui officia deserunt mollit
-						anim id est laborum.
+						While there exists brain analysis tools for adults, the
+						development of equivalent tools for infant brains are
+						lagging behind. The Infant FreeSurfer plugin, unlike the
+						standard FreeSurfer plugin, will allow researchers and
+						clinical workers to process and analyze infant brain
+						images to produce characteristic information about the
+						brains, such as volume, thickness, surface area, and
+						curvature. Developers can also choose to generate their
+						own workflow through a shell script that would run on a
+						local instance of ChRIS' CUBE.
 					</p>
 					<h2>Usage</h2>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-						sed do eiusmod tempor incididunt ut labore et dolore
-						magna aliqua. Ut enim ad minim veniam, quis nostrud
-						exercitation ullamco laboris nisi ut aliquip ex ea
-						commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu
-						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-						non proident, sunt in culpa qui officia deserunt mollit
-						anim id est laborum.
+						<ol>
+							<li>
+								Get the back-end of ChRIS running (further
+								instructions
+								<a href="http://chrisproject.org/join-us/get-chris-running">
+									{" "}
+									here
+								</a>
+								)
+							</li>
+							<li>
+								If you haven't already, log into the ChRIS
+								back-end through our login screen
+							</li>
+							<li>
+								Click the <b> Upload .NII or .DCM dataset </b>
+								button to upload a brain image
+							</li>
+							<li>
+								Afterwards, you can view the newly created feed
+								for the uploaded file as well as pre-existing
+								feeds associated to your CUBE account
+							</li>
+						</ol>
 					</p>
 				</Col>
 			</Row>
